b90b900f25998e9a92e8ee4d6d47dc950a11169f
SakyHank <sakyhank@gmail.com>
2014-11-05 11:14:27 +0100

    Initial commit

---
+++ app/cache/prod/twig/df/e5/19ee9e5f2f807a6691761206ef57a4aa71d0efba17546cd96e751e5c3803.php
@@ -0,0 +1,39 @@
+<?php
+
+/* ProjectLineOfWorkBundle:Default:index.html.twig */
+class __TwigTemplate_dfe519ee9e5f2f807a6691761206ef57a4aa71d0efba17546cd96e751e5c3803 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "ProjectLineOfWorkBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
